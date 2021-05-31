6fe32844752e0a85e1f3ad9ee235271123a5f956
mtiberghien <mathias.tiberghien@gmail.com>
2021-05-31 18:28:41 +0200

    Save and load optimizers separately

--- app/model/agent.py
+++ app/model/agent.py
@@ -2,11 +2,12 @@ import os
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
 from app.model.buffer import get_buffer_sample
 from app.model.settings import data_dir
 from os import path
 import json
+import pickle
+
 
 agent_settings_path = path.join(data_dir, 'agent.json')
 
@@ -48,6 +49,12 @@ def get_normalized_tensor(tensor):
     return tf.cast(tf.where(tf.math.is_nan(result), tf.zeros_like(result), result), dtype=tf.float32)
 
 
+@tf.function
+def update_target(target_weights, weights, tau):
+    for (a, b) in zip(target_weights, weights):
+        a.assign(b * tau + a * (1 - tau))
+
+
 class Agent:
     def __init__(self, use_p=True, use_i=True, use_d=True, use_iu=True, n_actions=2, alpha=0.001, beta=0.001,
                  gamma=0.99, tau=0.05, fc1=64, fc2=64, batch_size=256, min_action=0, max_action=100):
@@ -68,10 +75,14 @@ class Agent:
         self.target_actor_checkpoint_file = path.join(data_dir, 'target_actor_pid.h5')
         self.critic_checkpoint_file = path.join(data_dir, 'critic_pid.h5')
         self.target_critic_checkpoint_file = path.join(data_dir, 'target_critic_pid.h5')
-        self.actor = None
-        self.critic = None
-        self.target_actor = None
-        self.target_critic = None
+        self.actor_optimizer_file = path.join(data_dir, 'actor_optimizer.pkl')
+        self.critic_optimizer_file = path.join(data_dir, 'critic_optimizer.pkl')
+        self.actor_optimizer = Adam(learning_rate=self.alpha)
+        self.critic_optimizer = Adam(learning_rate=self.beta)
+        self.actor = self.build_actor()
+        self.critic = self.build_critic()
+        self.target_actor = self.build_actor()
+        self.target_critic = self.build_critic()
 
     def get_n_inputs(self):
         n_inputs = 0
@@ -92,24 +103,15 @@ class Agent:
             tau_actor = self.tau
         if tau_critic is None:
             tau_critic = self.tau
-        weights = []
-        targets = self.target_actor.weights
-        for i, weight in enumerate(self.actor.weights):
-            weights.append(weight * tau_actor + (1 - tau_actor) * targets[i])
-        self.target_actor.set_weights(weights)
-
-        weights = []
-        targets = self.target_critic.weights
-        for i, weight in enumerate(self.critic.weights):
-            weights.append(weight * tau_critic + (1 - tau_critic) * targets[i])
-        self.target_critic.set_weights(weights)
+        update_target(self.target_actor.variables, self.actor.variables, tau_actor)
+        update_target(self.target_critic.variables, self.critic.variables, tau_critic)
 
     def set_actor_weights(self, weights):
         weights = np.array(weights)[self.used_states].tolist()
         tf_weights = tf.convert_to_tensor([[[w] for w in weights]], dtype=tf.float32)
         self.actor.set_weights(tf_weights)
         self.target_actor.set_weights(tf_weights)
-        self.actor.save(self.actor_checkpoint_file)
+        self.actor.save_weights(self.actor_checkpoint_file)
         self.target_actor.save_weights(self.target_actor_checkpoint_file)
 
     def get_actor_weights(self, is_target=False):
@@ -125,20 +127,35 @@ class Agent:
                 i += 1
         return result
 
+    def save_optimizer(self, is_actor=True):
+        file_path = self.actor_optimizer_file if is_actor else self.critic_optimizer_file
+        optimizer = self.actor_optimizer if is_actor else self.critic_optimizer
+        weights_values = tf.keras.backend.batch_get_value(optimizer.weights)
+        with open(file_path, 'wb') as f:
+            pickle.dump(weights_values, f)
+
+    def load_optimizer(self, is_actor=True):
+        file_path = self.actor_optimizer_file if is_actor else self.critic_optimizer_file
+        if path.exists(file_path):
+            optimizer = self.actor_optimizer if is_actor else self.critic_optimizer
+            with open(file_path, 'rb') as f:
+                weight_values = pickle.load(f)
+            optimizer.set_weights(weight_values)
+
     def save_models(self, train_actor=True):
         if train_actor:
-            self.actor.save(self.actor_checkpoint_file)
-        self.critic.save(self.critic_checkpoint_file)
+            self.actor.save_weights(self.actor_checkpoint_file)
+            self.save_optimizer(True)
+        self.critic.save_weights(self.critic_checkpoint_file)
         self.target_actor.save_weights(self.target_actor_checkpoint_file)
         self.target_critic.save_weights(self.target_critic_checkpoint_file)
+        self.save_optimizer(False)
 
     def build_actor(self):
         actor = tf.keras.Sequential([
             tf.keras.layers.InputLayer(input_shape=(self.n_inputs,)),
             tf.keras.layers.Dense(1, use_bias=False, kernel_initializer=tf.keras.initializers.Ones())
         ])
-        actor.compile(optimizer=Adam(learning_rate=self.alpha))
-        actor(tf.convert_to_tensor([np.zeros(self.n_inputs)]))
         return actor
 
     def build_critic(self):
@@ -148,22 +165,16 @@ class Agent:
             tf.keras.layers.Dense(self.fc2, activation='relu'),
             tf.keras.layers.Dense(1)
         ])
-        critic.compile(optimizer=Adam(learning_rate=self.beta))
         return critic
 
     def load_model(self, checkpoint_file, is_target, is_actor):
         exists = path.exists(checkpoint_file)
-        if not exists or is_target:
-            if is_actor:
-                model = self.build_actor()
-            else:
-                model = self.build_critic()
+        if is_actor:
+            model = self.target_actor if is_target else self.actor
         else:
-            model = tf.keras.models.load_model(checkpoint_file)
-        if exists and is_target:
+            model = self.target_critic if is_target else self.critic
+        if exists:
             model.load_weights(checkpoint_file)
-
-        return model
 
     def reset_models(self):
         self.actor = self.build_actor()
@@ -175,10 +186,12 @@ class Agent:
     def build_models(self):
         need_update_network = not path.exists(self.target_actor_checkpoint_file)
 
-        self.actor = self.load_model(self.actor_checkpoint_file, False, True)
-        self.target_actor = self.load_model(self.target_actor_checkpoint_file, True, True)
-        self.critic = self.load_model(self.critic_checkpoint_file, False, False)
-        self.target_critic = self.load_model(self.target_critic_checkpoint_file, True, False)
+        self.load_model(self.actor_checkpoint_file, False, True)
+        self.load_model(self.target_actor_checkpoint_file, True, True)
+        self.load_model(self.critic_checkpoint_file, False, False)
+        self.load_model(self.target_critic_checkpoint_file, True, False)
+        self.load_optimizer(True)
+        self.load_optimizer(False)
 
         if need_update_network:
             self.update_network_parameters(tau_actor=1, tau_critic=1)
@@ -189,23 +202,16 @@ class Agent:
         output = relu(-relu(self.max_action - output) + self.max_action - self.min_action) + self.min_action
         return output
 
-    def learn(self, train_actor):
-        used_batch_size, state, action, reward, new_state, step = get_buffer_sample(batch_size=self.batch_size)
-        states = get_normalized_tensor(state[:, self.used_states])
-        actions = self.get_actor_saturated(states, self.actor, training=False)
-        rewards = tf.convert_to_tensor(reward, dtype=tf.float32)
-        new_states = get_normalized_tensor(new_state[:, self.used_states])
-        steps = get_normalized_tensor(step)
-        prev_steps = get_normalized_tensor(step-1)
-
+    @tf.function
+    def train(self, states, new_states, actions, rewards, steps, prev_steps, train_actor):
         with tf.GradientTape() as tape:
             target_actions = self.get_actor_saturated(new_states, self.target_actor, training=True)
             critic_value_ = self.target_critic(tf.concat([new_states, target_actions, steps], axis=1), training=True)
             critic_value = self.critic(tf.concat([states, actions, prev_steps], axis=1), training=True)
             target = rewards + self.gamma * critic_value_
-            critic_loss = keras.losses.MSE(target, critic_value)
+            critic_loss = tf.math.reduce_mean(tf.math.square(target - critic_value))
         critic_network_gradient = tape.gradient(critic_loss, self.critic.trainable_variables)
-        self.critic.optimizer.apply_gradients(zip(critic_network_gradient,
+        self.critic_optimizer.apply_gradients(zip(critic_network_gradient,
                                                   self.critic.trainable_variables))
         if train_actor:
             with tf.GradientTape() as tape:
@@ -216,8 +222,18 @@ class Agent:
             actor_network_gradient = tape.gradient(actor_loss, self.actor.trainable_variables)
             tf.debugging.check_numerics(actor_network_gradient, message="actor_loss:{}".format(actor_loss),
                                         name="training actor")
-            self.actor.optimizer.apply_gradients(zip(actor_network_gradient, self.actor.trainable_variables))
+            self.actor_optimizer.apply_gradients(zip(actor_network_gradient, self.actor.trainable_variables))
 
+    def learn(self, train_actor):
+        used_batch_size, state, action, reward, new_state, step = get_buffer_sample(batch_size=self.batch_size)
+        states = get_normalized_tensor(state[:, self.used_states])
+        actions = self.get_actor_saturated(states, self.actor, training=False)
+        rewards = tf.convert_to_tensor(reward, dtype=tf.float32)
+        new_states = get_normalized_tensor(new_state[:, self.used_states])
+        steps = get_normalized_tensor(step)
+        prev_steps = get_normalized_tensor(step-1)
+
+        self.train(states, new_states, actions, rewards, steps, prev_steps, train_actor)
         self.update_network_parameters(tau_actor=(None if train_actor else 0))
         return used_batch_size
 
